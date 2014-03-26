72039ddcbbc9643b52a5eb2e811efafd79ab315a
Nathanael Jones <nathanael.jones@gmail.com>
2014-03-26 15:23:28 -0400

    Add txn_type parsing support to Chase parsers

--- lib/reunion/parsers/parsers_credit_card.rb
+++ lib/reunion/parsers/parsers_credit_card.rb
@@ -1,5 +1,41 @@
 module Reunion
-  class ChaseJotCsvParser < ParserBase
+
+  class ChaseCsvParser < ParserBase
+
+    def parse_txn_type(type)
+      return nil if type.nil?
+      type = type.strip.downcase
+      case type.strip.downcase
+      when "sale"
+        :purchase
+      when "return"
+        :return
+      when "payment"
+        :transfer
+      else
+        nil
+      end 
+    end
+
+    def parse(text)
+
+      a = CSV.parse(text, csv_options)
+      # Type,Trans Date,Post Date,Description,Amount
+      # SALE,09/16/2013,09/18/2013,"ADOBE SYSTEMS, INC.",-32.09
+      {transactions: 
+        a.map { |l| 
+          {date:  Date.strptime(l[:post_date], '%m/%d/%Y'), 
+            description: l[:description], 
+            amount: parse_amount(l[:amount]), 
+            txn_type: parse_txn_type(l[:type]),
+            chase_type: l[:type] }
+        }.reverse
+      }
+    end
+  end
+
+  class ChaseJotCsvParser < ChaseCsvParser
+    
     def parse(text)
 
       #Jot has irregular line endings
@@ -17,6 +53,7 @@ module Reunion
           description: l[:merchant_name], 
           amount: -parse_amount(l[:amount]), 
           chase_type: l[:type],
+          txn_type: parse_txn_type(l[:type]),
           id: l[:transaction_id],
           chase_tag: l[:tag],
           discard_if_unmerged: true
@@ -28,22 +65,5 @@ module Reunion
       #Jot exports also have incorrect amounts , so merging can be hard.
       #Jot doesn't remove authorizations that are later modified or not posted
     end 
-  end
-
-  class ChaseCsvParser < ParserBase
-    def parse(text)
-
-      a = CSV.parse(text, csv_options)
-      # Type,Trans Date,Post Date,Description,Amount
-      # SALE,09/16/2013,09/18/2013,"ADOBE SYSTEMS, INC.",-32.09
-      {transactions: 
-        a.map { |l| 
-          {date:  Date.strptime(l[:post_date], '%m/%d/%Y'), 
-            description: l[:description], 
-            amount: parse_amount(l[:amount]), 
-            chase_type: l[:type] }
-        }.reverse
-      }
-    end
   end
 end 
