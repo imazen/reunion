f614d47384cc0500e64ed1225936cda7ae14af5d
Nathanael Jones <nathanael.jones@gmail.com>
2014-04-14 02:32:06 -0400

    Add QuarterlyReport class

--- lib/reunion/reports/by_year_report.rb
+++ lib/reunion/reports/by_year_report.rb
@@ -22,4 +22,36 @@ module Reunion
     end 
 
   end
+
+  class QuarterlyReport < Report
+
+    def initialze(slug)
+      super(slug)
+      @group_only = true
+    end 
+
+    def get_child_reports(datasource)
+      data = datasource.filter(&filter) if filter
+
+      years = data.results.uniq{|t| t.date.year}.map{|t| t.date.year}.compact
+      years.map do |year|
+        s = []
+        (1..4).each do |quarter|
+          startmonth = quarter * 3
+          endmonth = startmonth + 2
+          s << Report.new("q#{quarter}".to_sym,
+            filter: ->(t){t.date.month.between?(startmonth,endmonth)},
+            subreports: subreports.dup,
+            group_only: true)
+        end
+
+        Report.new(year.to_s.to_sym, 
+          filter: ->(t){ t.date.year == year}, 
+          subreports: s,
+          group_only: true)
+      end
+    end 
+
+  end
+
 end
