359b6ba8241c9ac36167729f22afeaa47014b694
Nathanael Jones <nathanael.jones@gmail.com>
2014-04-16 14:38:31 -0400

    Add permutation support to field_value_report

--- lib/reunion/reports/field_value_report.rb
+++ lib/reunion/reports/field_value_report.rb
@@ -1,28 +1,39 @@
  module Reunion
    class FieldValueReport < Report
-    def initialize(slug, field)
+    def initialize(slug, field, permutations: [nil])
       super(slug)
       @include_nil = true
       @include_all = true
       @field = field
       @nil_slug = :'[uncategorized]'
       @group_only = true
+      @permutations = permutations
     end 
-    attr_accessor :field, :include_nil, :nil_slug, :include_all, :export_all
+    attr_accessor :field, :include_nil, :nil_slug, :include_all, :export_all, :permutations
 
     def get_child_reports(datasource)
       data = datasource.filter(&filter) if filter
 
       field_values = data.results.uniq{|t| t[field]}.map{|t| t[field]}
+      include_nil = @include_nil && field_values.include?(nil)
+      field_values = field_values.compact.sort
+      field_values << nil if include_nil
       field_values << :all if include_all 
       reports = []
 
       field_values.each do |val|
         next if val.nil? && !include_nil
-        reports << Report.new(val.nil? ? nil_slug : val.to_sym, 
-          filter: ->(t){ t[field] == val || val == :all}, 
-          subreports: subreports.dup, 
-          options: {omit_export: val == :all})
+        (permutations || [nil]).each do |permutation|
+          slug = val.nil? ? nil_slug : val.to_sym
+          lambda = ->(t){ t[field] == val || val == :all}
+          #Modify slug and lambda for permuations
+          slug = "#{slug.to_s}_#{permutation[:suffix].to_s}".to_sym if permutation && permutation[:suffix] 
+          lambda = ->(t){ permutation[:lambda].call(t) && (t[field] == val || val == :all)} if permutation && permutation[:lambda] 
+          reports << Report.new(slug, 
+            filter: lambda, 
+            subreports: subreports.dup, 
+            options: {omit_export: val == :all})
+        end 
       end 
       reports
     end
