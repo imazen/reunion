45abad786c59a49630ed1c6570f10253282af28f
Aaron D. Valade <adv@alum.mit.edu>
2014-06-25 10:38:00 +0800

    added a schema to the overrides call

--- test/test_overrides.rb
+++ test/test_overrides.rb
@@ -5,28 +5,33 @@ module Reunion
   describe OverrideSet do
 
     before do
-     
+
       @txns = []
-      @txns << Transaction.new({:date => Date.parse('2014-01-01'), 
+      @txns << Transaction.new({:date => Date.parse('2014-01-01'),
                                 :amount => BigDecimal.new("20.00"),
                                 :description => "Something",
-                                :tags => [:a], 
+                                :tags => [:a],
                                 :account_sym => :bank})
-
-      @set = OverrideSet.new
+      @schema = Schema.new(date: DateField.new(readonly: true, critical: true),
+                           amount: AmountField.new(readonly: true, critical: true, default_value: 0),
+                           description: DescriptionField.new(readonly: true, default_value: ''),
+                           tags: TagsField.new,
+                           memo: DescriptionField.new,
+                           account_sym: SymbolField.new(readonly: true))
+      @set = OverrideSet.new(@schema)
       OverrideSet.set_subindexes(@txns)
       @set.set_override(@txns[0], {tags: [:b]})
-    end 
+    end
 
     it 'should apply overrides correctly' do
       @set.apply_all(@txns)
       assert_equal [:b], @txns[0].tags
-    end 
+    end
 
     it 'should round-trip the representation' do
-      serialized = @set.to_tsv_str 
-      deserialized = OverrideSet.from_tsv_str(serialized)
+      serialized = @set.to_tsv_str
+      deserialized = OverrideSet.from_tsv_str(serialized, @schema)
       assert_equal @set.overrides, deserialized.overrides
-    end 
+    end
   end
 end
