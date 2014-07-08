6ec9da07e1d7e17df36801d91fc4179c08ff1147
Nathanael Jones <nathanael.jones@gmail.com>
2014-07-08 15:25:40 +0200

    Allow calculations within quarterly and year reports.

--- lib/reunion/reports/by_year_report.rb
+++ lib/reunion/reports/by_year_report.rb
@@ -15,6 +15,7 @@ module Reunion
         Report.new(val.to_s.to_sym, 
           filter: ->(t){ t.date.year == val || val == :all_years}, 
           subreports: subreports.dup,
+          calculations: calculations.dup,
           group_only: subreports.count > 0,
           options: {omit_export: val == :all_years})
       end 
@@ -42,6 +43,7 @@ module Reunion
           s << Report.new("q#{quarter}".to_sym,
             filter: ->(t){t.date.month.between?(startmonth,endmonth)},
             subreports: subreports.dup,
+            calculations: calculations.dup,
             group_only: true)
         end
 
