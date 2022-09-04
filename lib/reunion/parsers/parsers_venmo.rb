1f8642064e0ced8b873710df5ce99d3f817cfdbf
Lilith River <lilith@imazen.io>
2022-09-04 02:15:45 -0600

    Complete venmo parsing

--- lib/reunion/parsers/parsers_venmo.rb
+++ lib/reunion/parsers/parsers_venmo.rb
@@ -1,105 +1,154 @@
-=begin
-  
-Type and ID of transaction (transfer, charge, adjustment, etc.)
-Amount
-Currency
-Fees
-Net amount
-Date created
-Date available
-Description
-Transfer ID
-Transfer date
 
-=end
-# transactions always need to be split up when the fee column is populated
-
-#type: transfer, charge, adjustment, refund 
-#Transaction type: adjustment, application_fee, application_fee_refund, charge, payment, payment_failure_refund, payment_refund, refund, transfer, transfer_cancel, transfer_failure, transfer_refund, or validation.
-#amount, fee, net
-# Crash if currency != USD
-
-
+# Account Statement - (@lilithriver) - March 27th to June 26th 2021 ,,,,,,,,,,,,,,,,,,
+# Account Activity,,,,,,,,,,,,,,,,,,
+# ,ID,Datetime,Type,Status,Note,From,To,Amount (total),Amount (tip),Amount (fee),Funding Source,Destination,Beginning Balance,Ending Balance,Statement Period Venmo Fees,Terminal Location,Year to Date Venmo Fees,Disclaimer
+# ,,,,,,,,,,,,,$8.00,,,,,
+# ,3240312810181230868,2021-03-29T18:43:08,Payment,Complete,Laser,Lilith River,Brenda Mendez,- $20.00,,,Visa Debit *0040,,,,,Venmo,,
+# ,3246271269313708750,2021-04-07T00:01:31,Payment,Complete,Cleaning,Lilith River,Veronica Cornelio,- $90.00,,,Visa Debit *0040,,,,,Venmo,,
 
 module Reunion
- class VenmoCsvParser < ParserBase
-
-
+  class VenmoCsvParser < ParserBase
     def parse_date(text)
       Date.iso8601(text)
+    rescue Date::Error
+      $stderr << "trying to parse date '#{text}'"
+      raise
     end 
 
     def parse(text)
-      a = CSV.parse(text,**csv_options)
-      {transactions: 
-        a.map { |l| 
+      csv_part = text.sub(/\AAccount Statement[^\n]+\nAccount Activity[^\n]+\n/, '')
+      
+      if csv_part.length == text.length
+        older_header = text.include?('ID,Datetime,Type,Status,Note,From,To,Amount (total),Amount (fee),Funding Source,Destination')
+        raise "Missing the Account Statement/Account Activity header for Venmo csv, got #{text[0..100]}" unless older_header
+      end 
+
+      a = CSV.parse(csv_part,**csv_options)
+      {transactions:
+        a.map do |l|
           parse_row(l)
-        }.flatten.reverse
+        end.flatten.reverse
       }
     end
 
+    def parse_txn_type(type)
+      return :charge if type ==  "charge" 
+
+      return :payment if type == "payment"
+
+      s = type.strip.downcase.to_sym
+      s = :refund if s == :'refunded transaction'
+      s = :payment if s == :'merchant transaction'
+      s = :transfer if s == :'standard transfer'
+      if [:payment, :charge, :refund, :transfer].include?(s)
+        s
+      else
+        raise "Unsupported Venmo transaction type #{type}. Please implement"
+      end
+    end
+    def parse_status(status)
+      return nil if status.nil?
+      return :complete if status ==  "Complete"
+
+      return :pending if status == "Pending"
+
+      s = status.strip.downcase.to_sym
+
+      # Because venmo, insane that it is, doesn't include failed transactions but leaves the status as 'failed' even for transient errors
+      s = :complete if s == :failed
+
+      # We don't care if it's refunded later, it was completed and affected the balance in the meantime
+      s = :complete if s == :refunded
+
+      s = :complete if s == :issued
+
+      if [:pending, :complete].include?(s)
+        s
+      else
+        raise "Unsupported Venmo transaction status #{status}. Please implement"
+      end 
+    end
+
+    #Venmo balance
+
+    def parse_venmo_amount(text)
+      return nil if text.nil? || text.empty?
+      parse_amount(text.gsub(/[ $]/,''))
+    end 
+
+
     def parse_row(l)
+
+      # Some rows have no datetime, just a balance
+      return [] if l[:datetime].nil? || l[:datetime].empty?
+
+      status = parse_status(l[:status])
+      # Skip anything pending
+      return [] if status == :pending
+
+      # Parse the transaction type (charge, payment, refund)
+      txn_type = parse_txn_type(l[:type])
+      
+      # Parse the date
       date = parse_date(l[:datetime])
 
+      # Parse amounts
+      total = parse_venmo_amount(l[:amount_total])
+      fee = parse_venmo_amount(l[:amount_fee]) || 0
+      tip = parse_venmo_amount(l[:amount_tip]) || 0
 
-      amount = parse_amount(l[:amount_total])
-      fee = parse_amount(l[:amount_fee])
-      net = amount - fee
+      
+      recipient = txn_type == :charge ? l[:from] : l[:to]
 
-      is_request = l[:type].downcase == "charge" 
-      raise "Invalid Venmo txn type #{l[:type]}" if !is_request && l[:type].downcase != "payment"
+      note = l[:note]
 
-      #skip txns that haven't cleared
-      if l[:status].nil? || l[:status].downcase != "complete"
-        return [] 
+      describe_exchange = txn_type == :charge ? "#{recipient} charged you via Venmo" : "#{recipient} was paid via Venmo"
+      
+      # You only ever get one of these two
+      # target_account = Venmo balance WHEN you receive funds
+      # target_account = something else WHEN you withdraw funds to checking, txn_type should be :transfer
+      source_account = l[:funding_source] || ''
+      target_account = l[:destination] || ''
+
+      # Parse funding source
+      # Amex Send Account - create offset transactions w/o transfer tag
+      # Venmo balance - treat these normally
+      # All others - create offset transactions with transfer tag
+
+      balance_txn = source_account.strip.downcase == "venmo balance" || target_account.strip.downcase == "venmo balance"
+      tag_as_transfer = source_account.strip.downcase != "amex send account" && target_account.strip.downcase != "amex send account"
+      
+      fee_desc = fee != 0 ? " (fee #{'%.2f' % fee})" : ''
+      desc = "#{describe_exchange}#{fee_desc} for: #{note}"
+      # Withdrawals to a bank account we handle differently
+      if txn_type == :transfer
+        return [{
+          date: date,
+          description: "Balance funds transferred to #{target_account}",
+          description2: note,
+          amount: total,
+          transfer: true
+        }]
       end 
 
-      from = l[:from]
-      to = l[:to]
-
-      source = l[:funding_source]
-      dest = l[:destination]
-
-
-      recipient = amount > 0 
-
-      desc =  l[:note]
-      desc2 = l[:type].downcase == "charge" ? "#{l[:from]} charged #{l[:to]} " : "TODO"
-
-
-
-
-      l[:type]
-
-      txn_type = parse_txn_type(l[:type])
-
-    
-      #if it lacks a transfer_date, then it probably hasn't landed in the bank yet
-      # adjustment fee = chargeback fee
       results = []
 
-      if fee != 0 then
+      unless balance_txn
+        # Create matching txn for withdrawal from funding source
         results << {
-          date: date,
-          description: desc,
-          amount: 0 - fee,
-          txn_type: :fee
+          date:date,
+          description: "VENMO FUNDING FOR* #{describe_exchange}, funded by #{source_account} for: #{note}",
+          amount: total * -1,
+          transfer: tag_as_transfer,
         }
-      end 
-      #id,Type,Source,Amount,Fee,Net,Currency,Created (UTC),Available On (UTC),Description,Customer Facing Amount,Customer Facing Currency,Transfer,Transfer Date (UTC)
-      #txn_19gkmR2kdGSXuqwcnYpNleXL,transfer,tr_19gkmR2kdGSXuqwcX85dZLPZ,-824.08,0.00,-824.08,usd,2017-01-28 01:13,2017-01-29 00:00,STRIPE TRANSFER,,,,
-      #txn_18bB5g2kdGSXuqwcVm1fYE09,refund,ch_18BnYG2kdGSXuqwcGJj4i9MG,-249.00,-7.52,-241.48,usd,2016-07-25 15:34,2016-07-25 15:34,REFUND FOR CHARGE (Spree Order ID: R023318196-BGR5XETC),-249.00,usd,tr_18bNa22kdGSXuqwcZWRXdGRb,2016-07-27 00:00
-      #txn_18XNw42kdGSXuqwc9ADfcRNe,transfer,tr_18XNw42kdGSXuqwcPblCB9xV,-824.08,0.00,-824.08,usd,2016-07-15 04:28,2016-07-16 00:00,STRIPE TRANSFER,,,tr_18XNw42kdGSXuqwcPblCB9xV,2016-07-18 00:00
-      #txn_18X4XT2kdGSXuqwcBrHTyg2s,charge,ch_18X4XS2kdGSXuqwcFAw6Jwos,849.00,24.92,824.08,usd,2016-07-14 07:45,2016-07-16 00:00,Spree Order ID: R102903265-QUTV7T4D,849.00,usd,tr_18XNw42kdGSXuqwcPblCB9xV,2016-07-18 00:00
-      #txn_18X1Qq2kdGSXuqwc07tcbbN7,transfer,tr_18X1Qq2kdGSXuqwc3t8R373F,864.00,0.00,864.00,usd,2016-07-14 04:26,2016-07-15 00:00,STRIPE TRANSFER,,,tr_18X1Qq2kdGSXuqwc3t8R373F,2016-07-15 00:00
-      #txn_18Wt2c2kdGSXuqwcAk3BN2bS,adjustment,ch_18RUCq2kdGSXuqwcxuiNjYA4,-849.00,15.00,-864.00,usd,2016-07-13 19:29,2016-07-13 19:29,Chargeback withdrawal for ch_18RUCq2kdGSXuqwcxuiNjYA4,,,tr_18X1Qq2kdGSXuqwc3t8R373F,2016-07-15 00:00
+      end
+
       results << {
           date: date,
           description: desc,
-          amount: amount,
-          txn_type: txn_type
+          amount: total
         }
       results
-    end 
+    end
   end
 end
